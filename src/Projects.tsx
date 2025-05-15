0a9dfee7bae3e940630f076b850ab86c79f9b38b
Andrew Brown <andrewbrownconsult@gmail.com>
2025-05-15 06:15:26 +0100

    add project links

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -5,20 +5,20 @@ export default function Projects() {
         <div id="projects-sect">
             <h1 id="title">Projects</h1>
             <div id="projects-grid">
-                <div className="projectHero">
+                <a className="projectHero" href="https://pool-table-ivory.vercel.app">
                     <h1>Pool Game</h1>
                     <img src="pool.png"></img>
-                </div>
-                <div className="projectHero">
+                </a>
+                <a className="projectHero" href="https://oscilloscope-pcfs0ftvg-andb123s-projects.vercel.app">
                     <h1>Oscilloscope</h1>
                     <img src="oscilloscope.png"></img>
-                </div>
-                <div className="projectHero">
+                </a>
+                <a className="projectHero" href="https://washing-machine-tau.vercel.app">
                     <h1>Washing Machine</h1>
                     <img src="washingMachine.png"></img>
-                </div>
+                </a>
             </div>
 
-        </div>
+        </div >
     );
 }
